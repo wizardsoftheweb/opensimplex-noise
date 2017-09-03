3be395839ccf33ae47c986b1d13628654678d875
CJ Harries <cj@wizardsoftheweb.pro>
2017-09-03 17:08:47 -0500

    Add an LCG init Java benchmark

---
+++ benchmarks/java/LcgInitialization.java
@@ -0,0 +1,17 @@
+public class LcgInitialization extends Benchmark
+{
+    void benchmarkCallback()
+    {
+        long seed = 0L;
+        seed = seed * 6364136223846793005L + 1442695040888963407L;
+        seed = seed * 6364136223846793005L + 1442695040888963407L;
+        seed = seed * 6364136223846793005L + 1442695040888963407L;
+    }
+
+    public static void main(String[] argv)
+    throws Exception
+    {
+        LcgInitialization benchmark = new LcgInitialization();
+        benchmark.run(argv);
+    }
+}
