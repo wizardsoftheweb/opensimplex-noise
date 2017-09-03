258976767b35bc1b4c09aec01a4f66c66deba41d
CJ Harries <cj@wizardsoftheweb.pro>
2017-09-03 17:54:56 -0500

    Add abstract Benchmark class

---
+++ benchmarks/java/Benchmark.java
@@ -0,0 +1,25 @@
+public abstract class Benchmark
+{
+    public static final int MAX_COUNT = 100;
+
+    abstract void benchmarkCallback();
+
+    public void run(String[] argv)
+    throws Exception
+    {
+        int maxCount = 0;
+        try {
+            maxCount = Integer.parseInt(argv[0]);
+        } catch (Exception e) {
+            maxCount = MAX_COUNT;
+        }
+        long start = System.nanoTime();
+        for (int count = 0; count < maxCount; count++)
+        {
+            this.benchmarkCallback();
+        }
+        long elapsed = System.nanoTime() - start;
+        long elapsedMicro = elapsed / 1000L;
+        System.out.println(elapsedMicro);
+    }
+}
